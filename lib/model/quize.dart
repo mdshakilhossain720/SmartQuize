149c1dcff2f9484b3acd1e011be96415af9fce4e
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-02-20 18:24:15 +0600

    quize

--- lib/model/quize.dart
+++ lib/model/quize.dart
@@ -16,4 +16,45 @@ class Quize {
     this.createdAt,
     this.updatedAt,
   });
+
+  factory Quize.fromMap(String id, Map<String, dynamic> map) {
+    return Quize(
+      id: id,
+      title: map['title'] ?? '',
+      timeLimit: map['timeLimit'] ?? 0,
+      questions:((map['questions'] ?? []) as List).map((e) => Question.fromMap(e)).toList(),
+      createdAt: map['createdAt'],
+      updatedAt: map['updatedAt'],
+    );
+  }
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'timeLimit': timeLimit,
+      'questions': questions.map((e) => e.toMap()).toList(),
+      
+      'updatedAt': DateTime.now(),
+    };
+  }
+
+  Quize copyWith({
+    String? id,
+    String? title,
+    int? timeLimit,
+    List<Question>? questions,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) {
+    return Quize(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      timeLimit: timeLimit ?? this.timeLimit,
+      questions: questions ?? this.questions,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
+
+   
 }
